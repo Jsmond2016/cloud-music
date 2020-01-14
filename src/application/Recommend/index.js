95154236d8f8a66539839cb03899530ea7c21c4d
黄锦 <jin.huang02@hand-china.com>
2020-01-14 16:03:21 +0800

    完成播放器6

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,67 +1,65 @@
 import React, { useEffect } from 'react';
-import { forceCheck } from 'react-lazyload';
-import { renderRoutes } from 'react-router-config';
 import Slider from '../../components/slider/';
 import { connect } from "react-redux";
+import {forceCheck} from 'react-lazyload';
 import * as actionTypes from './store/actionCreators';
 import RecommendList from '../../components/list/';
 import Scroll from '../../baseUI/scroll/index';
-import Loading from '../../baseUI/loading/index';
 import { Content } from './style';
+import Loading from '../../baseUI/loading/index';
+import { renderRoutes } from 'react-router-config';
 
-function Recommend (props){
-  const { bannerList, recommendList, enterLoading } = props;
+function Recommend(props){
+  const { bannerList, recommendList, enterLoading, songsCount } = props;
 
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
-  useEffect (() => {
-    // 如果页面有数据，则不发请求
-    //immutable 数据结构中长度属性 size
-    if (!bannerList.size){
-      getBannerDataDispatch ();
+  useEffect(() => {
+    if(!bannerList.size){
+      getBannerDataDispatch();
     }
-    if (!recommendList.size){
-      getRecommendListDataDispatch ();
+    if(!recommendList.size){
+      getRecommendListDataDispatch();
     }
-    //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
-  const bannerListJS = bannerList ? bannerList.toJS () : [];
-  const recommendListJS = recommendList ? recommendList.toJS () :[];
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() :[];
 
   return (
-    <Content>
-      <Scroll className="list" onScroll={forceCheck}>
+    <Content play={songsCount}>
+      <Scroll onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
       { enterLoading ? <Loading></Loading> : null }
-      { renderRoutes (props.route.routes) }
+      { renderRoutes(props.route.routes) }
     </Content> 
   );
 }
 
-// 映射 Redux 全局的 state 到组件的 props 上
+// 映射Redux全局的state到组件的props上
 const mapStateToProps = (state) => ({
-  // 不要在这里将数据 toJS
-  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
-  bannerList: state.getIn (['recommend', 'bannerList']),
-  recommendList: state.getIn (['recommend', 'recommendList']),
-  enterLoading: state.getIn (['recommend', 'enterLoading']),
+  // 不要再这里将数据toJS,不然每次diff比对props的时候都是不一样的引用，还是导致不必要的重渲染, 属于滥用immutable
+  bannerList: state.getIn(['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn(['recommend', 'enterLoading']),//简单数据类型不需要调用toJS
+  songsCount: state.getIn(['player', 'playList']).size,//尽量减少toJS操作，直接取size属性就代表了list的长度
 });
-// 映射 dispatch 到 props 上
+// 映射dispatch到props上
 const mapDispatchToProps = (dispatch) => {
   return {
-    getBannerDataDispatch () {
-      dispatch (actionTypes.getBannerList ());
+    getBannerDataDispatch() {
+      dispatch(actionTypes.getBannerList());
     },
-    getRecommendListDataDispatch () {
-      dispatch (actionTypes.getRecommendList ());
+    getRecommendListDataDispatch() {
+      dispatch(actionTypes.getRecommendList());
     },
   }
 };
 
-// 将 ui 组件包装成容器组件
-export default connect (mapStateToProps, mapDispatchToProps)(React.memo (Recommend));
+// 将ui组件包装成容器组件
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend));
--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useContext} from 'react';
 import Horizen from '../../baseUI/horizen-item';
 import { categoryTypes, alphaTypes } from '../../api/config';
+import { CategoryDataContext } from './data';
 import { 
   NavContainer,
   ListContainer,
@@ -21,12 +22,11 @@ import  LazyLoad, {forceCheck} from 'react-lazyload';
 import Scroll from './../../baseUI/scroll/index';
 import {connect} from 'react-redux';
 import Loading from '../../baseUI/loading';
-import { CHANGE_CATEGORY, CHANGE_ALPHA, CategoryDataContext, Data } from './data';
+import { CHANGE_CATEGORY, CHANGE_ALPHA, Data } from './data';
 import { renderRoutes } from 'react-router-config';
 
-
 function Singers(props) {
-  const { singerList, enterLoading, pullUpLoading, pullDownLoading, pageCount } = props;
+  const { singerList, enterLoading, pullUpLoading, pullDownLoading, pageCount, songsCount } = props;
 
   const { getHotSingerDispatch, updateDispatch, pullDownRefreshDispatch, pullUpRefreshDispatch } = props;
 
@@ -40,6 +40,9 @@ function Singers(props) {
     }
     // eslint-disable-next-line
   }, []);
+  const enterDetail = (id)  => {
+    props.history.push(`/singers/${id}`);
+  };
 
   let handleUpdateAlpha = (val) => {
     dispatch({type: CHANGE_ALPHA, data: val});
@@ -59,10 +62,6 @@ function Singers(props) {
     pullDownRefreshDispatch(category, alpha);
   };
 
-  const enterDetail = (id)  => {
-    props.history.push (`/singers/${id}`);
-  };
-
   const renderSingerList = () => {
     const list = singerList ? singerList.toJS(): [];
     return (
@@ -70,7 +69,7 @@ function Singers(props) {
         {
           list.map((item, index) => {
             return (
-              <ListItem key={item.accountId+""+index} onClick={() => enterDetail (item.id)}>
+              <ListItem key={item.accountId+""+index} onClick={() => enterDetail(item.id)}>
                 <div className="img_wrapper">
                   <LazyLoad placeholder={<img width="100%" height="100%" src={require('./singer.png')} alt="music"/>}>
                     <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
@@ -91,7 +90,7 @@ function Singers(props) {
           <Horizen list={categoryTypes} title={"分类(默认热门):"} handleClick={(val) => handleUpdateCatetory(val)} oldVal={category}></Horizen>
           <Horizen list={alphaTypes} title={"首字母:"} handleClick={val => handleUpdateAlpha(val)} oldVal={alpha}></Horizen>
         </NavContainer> 
-        <ListContainer>
+        <ListContainer play={songsCount}>
           <Scroll
             pullUp={ handlePullUp }
             pullDown = { handlePullDown }
@@ -104,7 +103,7 @@ function Singers(props) {
           <Loading show={enterLoading}></Loading>
         </ListContainer>
       </Data>
-      { renderRoutes (props.route.routes) }
+      { renderRoutes(props.route.routes) }
     </div>
   )
 }
@@ -114,7 +113,8 @@ const mapStateToProps = (state) => ({
   enterLoading: state.getIn(['singers', 'enterLoading']),
   pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
   pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
-  pageCount: state.getIn(['singers', 'pageCount'])
+  pageCount: state.getIn(['singers', 'pageCount']),
+  songsCount: state.getIn(['player', 'playList']).size
 });
 const mapDispatchToProps = (dispatch) => {
   return {
